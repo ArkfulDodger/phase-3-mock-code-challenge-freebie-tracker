e9ec38960cc34afd272e427d6a6a407cecc8d8f4
Noah Reece <noah.w.reece@gmail.com>
2022-04-10 22:55:06 -0400

    completed

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,3 +1,12 @@
 class Company < ActiveRecord::Base
+    has_many :freebies
+    has_many :devs, through: :freebies
 
+    def give_freebie(dev, item_name, value)
+        self.freebies << Freebie.new(item_name: item_name, value: value, dev: dev)
+    end
+
+    def self.oldest_company
+        Company.all.min { |a, b| a.founding_year<=>b.founding_year }
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,10 +12,16 @@ Dev.create(name: "Gazorpazop")
 
 puts "Creating freebies..."
 
-# ***************************************************************
-# * TODO: create freebies! Remember, a freebie belongs to a dev *
-# * and a freebie belongs to a company.                         *
-# ***************************************************************
-# Create freebies Here
+Freebie.create(item_name: "bucket", value: 5, company_id: 1, dev_id: 1)
+Freebie.create(item_name: "bucket", value: 5, company_id: 1, dev_id: 2)
+Freebie.create(item_name: "bucket", value: 5, company_id: 1, dev_id: 3)
+Freebie.create(item_name: "book", value: 20, company_id: 2, dev_id: 4)
+Freebie.create(item_name: "paper", value: 50, company_id: 3, dev_id: 1)
+Freebie.create(item_name: "paper", value: 50, company_id: 3, dev_id: 2)
+Freebie.create(item_name: "paper", value: 50, company_id: 3, dev_id: 3)
+Freebie.create(item_name: "paper", value: 50, company_id: 3, dev_id: 4)
+Freebie.create(item_name: "drugs", value: 100, company_id: 4, dev_id: 1)
+Freebie.create(item_name: "drugs", value: 100, company_id: 4, dev_id: 2)
+
 
 puts "Seeding done!"
